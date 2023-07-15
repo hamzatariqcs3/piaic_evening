e680af6b90397c74da22b19e9c0e94560a24f974
hamzatariqcs3 <hamzatariqcs3@gmail.com>
2023-07-16 00:52:31 +0500

    assignment 2

--- Q1/class2/index.js
+++ Q1/class2/index.js
@@ -4,19 +4,33 @@ let na1 = 23; // 1st variabe
 let na2 = 32; // 2nd variable
 let na3 = 5; // 3rd variable
 let sum = na1 + na2 + na3; //adding numbers using the +operator.
-console.log(sum); // Output:60
+console.log(`Sum of ${na1} + ${na2} + ${na3} = ${sum}`); // Output:60
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
-// in bellow line i'm tring to addition 3 values
+// in bellow line i'm tring to subtraction 3 values
 let ns1 = 100; // 1st variabe 
 let ns2 = 35; // 2nd variable
 let ns3 = 5; // 3rd variable
-let sub = ns1 - ns2 - ns3; //subtract numbers using the +operator.
-console.log(sub); // Output:60
+let sub = ns1 - ns2 - ns3; //subtraction numbers using the -operator.
+console.log(`Subtraction of ${ns1} - ${ns2} -${ns3}  = ${sub}`); // Output:60
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
-// in bellow line i'm tring to multiplay 2 values
+// in bellow line i'm tring to multiply 2 values
 let nm1 = 15; // 1st variabe 
 let nm2 = 2; // 2nd variable 
-let mul = nm1 * nm2 * 2; //multiplay numbers using the +operator.
-console.log(mul); // Output:60
+let mul = nm1 * nm2 * 2; //multiplication numbers using the *operator.
+console.log(`Multiplication of ${nm1} * ${nm2} * 2  = ${mul}`); // Output:60
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+// in bellow line i'm tring to division 2 values
+let nd1 = 120; // 1st variabe 
+let nd2 = 2; // 2nd variable 
+let div = nd1 / nd2; //division numbers using the +operator.
+console.log(`division of ${nd1} / ${nd2}   = ${div}`); // Output:60
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+// in bellow line i'm tring to use difference operators
+let n1 = 3; // 1st variabe 
+let n2 = 2; // 2nd variable 
+let val = ((((n1 + n2) * (n1 - n2) * 40) / 4) + 50) - (80 / n2); //division numbers using the +operator.
+console.log(`Result of ((((${n1} + ${n2}) * (${n1} - ${n2})  * 40 ) / 4) + 50 ) -( 80 / ${n2}) =  ${val}`); // Output:60
