6ac8252bae24fcb1838ba1f0680b04206c0dec20
hamzatariqcs3 <hamzatariqcs3@gmail.com>
2023-07-16 00:14:31 +0500

    class 2 assignment
    class 2 assignment

---
+++ Q1/class2/index.ts
@@ -0,0 +1,31 @@
+// in bellow line i'm tring to addition 3 values
+let na1 = 23 ; // 1st variabe 
+let na2 = 32 ;  // 2nd variable
+let na3 = 5 ;  // 3rd variable
+
+let sum = na1 + na2 + na3; //adding numbers using the +operator.
+
+console.log(sum);   // Output:60
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+// in bellow line i'm tring to addition 3 values
+let ns1 = 100 ; // 1st variabe 
+let ns2 = 35 ;  // 2nd variable
+let ns3 = 5 ;  // 3rd variable
+
+let sub = ns1 - ns2 - ns3; //subtract numbers using the +operator.
+
+console.log(sub);   // Output:60
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+// in bellow line i'm tring to multiplay 2 values
+let nm1 = 15 ; // 1st variabe 
+let nm2 = 2 ;  // 2nd variable 
+
+let mul = nm1 * nm2 * 2 ; //multiplay numbers using the +operator.
+
+console.log(mul);   // Output:60
